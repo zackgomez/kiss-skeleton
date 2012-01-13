9f7a116016e389eecd4d4e4045452a60cef75219
Zack Gomez <zacharyg@caltech.edu>
2012-01-12 20:30:48 -0800

    Added support for reading/dumping bones

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <GL/glut.h>
 #include <math.h>       /* for cos(), sin(), and sqrt() */
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <map>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -26,9 +28,14 @@ struct Bone
         parent(parnt)
     {
     }
+
+    Bone(const std::string &line);
 };
 
 void renderBone(Bone *bone);
+void printSkeleton(Bone *skeleton);
+Bone* readBone(const std::string &bonestr, Bone *skeleton,
+        std::map<std::string, Bone*> &bonemap);
 
 
 int windowWidth = 800, windowHeight = 600;
@@ -86,6 +93,8 @@ void keyboard(GLubyte key, GLint x, GLint y)
     // Quit on ESC
     if (key == 27)
         exit(0);
+    if (key == 'd')
+        printSkeleton(skeleton);
 }
 
 int main(int argc, char **argv)
@@ -119,24 +128,23 @@ int main(int argc, char **argv)
     // --------------------
     // START MY SETUP
     // --------------------
-    
-    Bone *head = new Bone("head", 2.f, glm::vec3(0.f, 3.f, 0.f), glm::vec4(0.f, 0.f, 1.f, -90.f), NULL);
-    Bone *backh = new Bone("backh", 3.f, glm::vec3(0.f, 0.f, 0.f), glm::vec4(0.f, 0.f, 0.f, 0.f), head);
-    head->children.push_back(backh);
-    Bone *backl = new Bone("backl", 3.f, glm::vec3(0.f, 0.f, 0.f), glm::vec4(0.f, 0.f, 0.f, 0.f), backh);
-    head->children.push_back(backl);
 
-    Bone *rarm = new Bone("rarm", 3.f, glm::vec3(0.f, 0.f, 0.f), glm::vec4(0.f, 0.f, 1.f, 60.f), head);
-    Bone *larm = new Bone("larm", 3.f, glm::vec3(0.f, 0.f, 0.f), glm::vec4(0.f, 0.f, 1.f, -60.f), head);
-    head->children.push_back(rarm);
-    head->children.push_back(larm);
+    char *bonestrs[7] = {
+        "head 0 3 0 0 0 1 -90 2 NULL", 
+        "backh 0 0 0 0 0 0 0 3 head", 
+        "backl 0 0 0 0 0 0 0 3 backh", 
+        "rleg 0 0 0 0 0 1 20 3 backl", 
+        "lleg 0 0 0 0 0 1 -20 3 backl", 
+        "rarm 0 0 0 0 0 1 60 3 head", 
+        "larm 0 0 0 0 0 1 -60 3 head", 
+    };
 
-    Bone *rleg = new Bone("rleg", 3.f, glm::vec3(0.f, 0.f, 0.f), glm::vec4(0.f, 0.f, 1.f, 20.f), backl);
-    Bone *lleg = new Bone("lleg", 3.f, glm::vec3(0.f, 0.f, 0.f), glm::vec4(0.f, 0.f, 1.f, -20.f), backl);
-    backl->children.push_back(rleg);
-    backl->children.push_back(lleg);
-
-    skeleton = head;
+    skeleton = NULL;
+    std::map<std::string, Bone*> bonemap;
+    for (int i = 0; i < 7; i++)
+    {
+        skeleton = readBone(bonestrs[i], skeleton, bonemap);
+    }
 
     // --------------------
     // END MY SETUP
@@ -150,8 +158,6 @@ void renderBone(Bone *bone)
 {
     if (!bone)
         return;
-
-    std::cout << "Rendering bone: " << bone->name << '\n';
 
     glPushMatrix();
 
@@ -174,4 +180,47 @@ void renderBone(Bone *bone)
     }
 
     glPopMatrix();
+}
+
+Bone* readBone(const std::string &bonestr, Bone *skeleton, std::map<std::string, Bone*> &bonemap)
+{
+    std::stringstream ss(bonestr);
+    std::string name, parentname;
+    float x, y, z, a, rotx, roty, rotz, length;
+    ss >> name >> x >> y >> z >> rotx >> roty >> rotz >> a >> length >> parentname;
+    if (!ss)
+    {
+        std::cerr << "Could not read bone from string: '" << bonestr << "'\n";
+        return NULL;
+    }
+
+    if (parentname == "NULL")
+    {
+        assert(!skeleton);
+        Bone *newbone = new Bone(name, length, glm::vec3(x, y, z), glm::vec4(rotx, roty, rotz, a), NULL);
+        bonemap[name] = newbone;
+        return newbone;
+    }
+
+    assert(skeleton);
+    assert(bonemap.find(parentname) != bonemap.end());
+    Bone *parent = bonemap[parentname];
+
+    Bone *newbone = new Bone(name, length, glm::vec3(x, y, z), glm::vec4(rotx, roty, rotz, a), parent);
+    parent->children.push_back(newbone);
+    bonemap[name] = newbone;
+    return skeleton;
+}
+
+void printSkeleton(Bone *cur)
+{
+    if (cur == NULL)
+        return;
+
+    std::cout << cur->name << ' ' << cur->pos[0] << ' ' << cur->pos[1] << ' ' << cur->pos[2] << ' '
+        << cur->rot[0] << ' ' << cur->rot[1] << ' ' << cur->rot[2] << ' ' << cur->rot[3] << ' '
+        << cur->length << ' ' << (cur->parent == NULL ? "NULL" : cur->parent->name) << '\n';
+
+    for (size_t i = 0; i < cur->children.size(); i++)
+        printSkeleton(cur->children[i]);
 }
